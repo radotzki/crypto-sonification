fa748d8441d81dcfb9806b996b5f51288b41a75b
Itay Radotzki <radotzki@gmail.com>
2017-12-30 18:52:21 +0200

    play sound

--- client/index.js
+++ client/index.js
@@ -1,7 +1,48 @@
 const ws = new WebSocket('ws://0.0.0.0:8090');
+import { initSound, playRandomSwell, playPitchAtVolume } from './sound';
 
-ws.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-    console.log(data);
-};
+main();
 
+function main() {
+    initSound();
+    subscribeToMessages();
+}
+
+function subscribeToMessages() {
+    ws.onmessage = event => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+
+        if (data.type === 'tx') {
+            playTransaction(data);
+        } else if (data.type === 'block') {
+            playBlock(data);
+        }
+    };
+}
+
+function playTransaction(tx) {
+    var max = 1;
+    var minVolume = 0.3;
+    var maxVolume = 0.7;
+    var volume = tx.value / (max / (maxVolume - minVolume)) + minVolume;
+    if (volume > maxVolume)
+        volume = maxVolume;
+
+    var maxPitch = 100.0;
+    // We need to use a log that makes it so that maxBitcoins reaches the maximum pitch.
+    // Well, the opposite of the maximum pitch. Anyway. So we solve:
+    // maxPitch = log(maxBitcoins + logUsed) / log(logUsed)
+    // For maxPitch = 100 (for 100%) and maxBitcoins = 1000, that gives us...
+    var logUsed = 1.0715307808111486871978099;
+    // So we find the smallest value between log(bitcoins + logUsed) / log(logUsed) and our max pitch...
+    var pitch = Math.min(maxPitch, Math.log(tx.value + logUsed) / Math.log(logUsed));
+    // ...we invert it so that a bigger transaction = a deeper noise...
+    pitch = maxPitch - pitch;
+
+    playPitchAtVolume(volume, pitch);
+}
+
+function playBlock(block) {
+    playRandomSwell();
+}
--- client/sound.js
+++ client/sound.js
@@ -1,122 +1,65 @@
-import * as Howler from 'howler';
+const Howler = require('howler');
 
-var globalVolume = 50;
-var globalScalePitch;
-var globalBank;
-var globalShowDollar = false;
+let sounds;
+let swells;
+let currentNotes = 0;
+let noteTimeout = 500;
 
-Howler.volume(globalVolume * 0.01);
+export function initSound() {
+    for (let i = 1; i <= 22; i++) {
+        let istring = zeroPad(i, 3);
+        let newSound = new Howl({
+            src: [
+                "sounds/celesta/" + "celesta" + istring + ".ogg",
+                "sounds/celesta/" + "celesta" + istring + ".mp3"
+            ],
+            autoplay: false
+        });
+        sounds.push(newSound);
+    }
 
-
-export function Sound() {
-
+    for (let i = 1; i <= 3; i++) {
+        let newSound = new Howl({
+            src: [
+                "sounds/swells/swell" + i + ".ogg",
+                "sounds/swells/swell" + i + ".mp3"
+            ],
+            autoplay: false
+        });
+        swells.push(newSound);
+    }
 }
-
-var soundBank = [];
-// the second number is the number of sound files available
-soundBank[0] = ["celesta", 22];
-soundBank[1] = ["planet", 33];
-soundBank[2] = ["wikki", 13];
 
 function zeroPad(num, places) {
     var zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;
 }
 
-Sound.loadup = function () {
-    var newSound;
-    var istring;
-
-    // sound0 = celesta
-    sound0 = [];
-
-    // sound1 = planet
-    sound1 = [];
-
-    // sound2 = celestaB
-    sound2 = [];
-
-    // String swells0, for blocks
-    swells0 = [];
-
-    // String swells1, for blocks
-    swells1 = [];
-
-    // String swells2, for blocks
-    swells2 = [];
-
-    // These variables are populated (and their sounds loaded) by the Sound.change function below.
-};
-
-
-
-Sound.init = function () {
-    Sound.change(0);
-};
-
-Sound.change = function (instrument_number) {
-    var musicianString = "Donate to instrument creator: ";
-    var i;
-    // INSTRUMENT 0
-    // Load sound and swells if not already loaded
-    if (sound0.length === 0) {
-        for (i = 1; i <= 22; i++) {
-            istring = zeroPad(i, 3);
-            newSound = new Howl({
-                urls: ["sounds/celesta/" + "celesta" + istring + ".ogg",
-                "sounds/celesta/" + "celesta" + istring + ".mp3"],
-                autoplay: false
-            });
-            sound0.push(newSound);
-        }
-    }
-    if (swells0.length === 0) {
-        for (i = 1; i <= 3; i++) {
-            newSound = new Howl({
-                urls: ["sounds/swells0/swell" + i + ".ogg",
-                "sounds/swells0/swell" + i + ".mp3"],
-                autoplay: false
-            });
-            swells0.push(newSound);
-        }
-    }
-    currentSound = sound0;
-    currentSwells = swells0;
-};
-
-var currentNotes = 0;
-var noteTimeout = 500;
-
-Sound.playRandomAtVolume = function (volume) {
-    if (globalMute)
-        return;
-
+export function playRandomAtVolume(volume) {
     var randomPitch = Math.floor(Math.random() * 100);
-    Sound.playPitchAtVolume(volume, randomPitch);
+    playPitchAtVolume(volume, randomPitch);
 };
 
-Sound.playPitchAtVolume = function (volume, pitch) {
-    if (globalMute)
-        return;
+export function playPitchAtVolume(volume, pitch) {
     // Find the index corresponding to the requested pitch
-    var index = Math.floor(pitch / 100.0 * currentSound.length);
+    var index = Math.floor(pitch / 100.0 * sounds.length);
     //console.log("Pitch: " + pitch);
 
     // Here we fuzz the index a bit to prevent the same sound
     // from being heard over and over again, which gets annoying
     var fuzz = Math.floor(Math.random() * 4) - 2;
     index += fuzz;
-    index = Math.min(currentSound.length - 1, index);
+    index = Math.min(sounds.length - 1, index);
     index = Math.max(0, index);
 
     //console.log("Fuzz: " + fuzz);
     //console.log("Index: " + index);
 
 
-    //var readyState = currentSound[index].get("readyState");
+    //var readyState = sounds[index].get("readyState");
     if (currentNotes < 5) {
-        currentSound[index].volume(volume);
-        currentSound[index].play();
+        sounds[index].volume(volume);
+        sounds[index].play();
         currentNotes++;
         setTimeout(function () {
             currentNotes--;
@@ -125,18 +68,15 @@ Sound.playPitchAtVolume = function (volume, pitch) {
 };
 
 var lastBlockSound = -1;
-Sound.playRandomSwell = function () {
-    if (globalMute)
-        return;
-
+export function playRandomSwell() {
     var randomIndex;
     do {
-        randomIndex = Math.floor(Math.random() * currentSwells.length);
+        randomIndex = Math.floor(Math.random() * swells.length);
     } while (randomIndex == lastBlockSound);
 
     lastBlockSound = randomIndex;
 
     //var readyState = this.swells[randomIndex].get("readyState");
     //if (readyState >= 2)
-    currentSwells[randomIndex].play();
+    swells[randomIndex].play();
 };
